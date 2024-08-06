498ccc0de446f2888b084df7e9e80e0dd2385628
YoelK20 <166104713+YoelK20@users.noreply.github.com>
2024-08-06 15:07:02 +0700

    Setup Connection to MongoDB & Setup helper Done

--- server/app.js
+++ server/app.js
@@ -1,17 +1,34 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
-require("dotenv").config();
+const { mongoConnect } = require("./config/mongoConnection");
 
 const server = new ApolloServer({
   introspection: true,
 });
 
 (async () => {
-  const { url } = await startStandaloneServer(server, {
-    listen: {
-      port: 4000,
-    },
-  });
+  try {
+    await mongoConnect()
+    const { url } = await startStandaloneServer(server, {
+      context: async ({req, res}) => {
+        return {
+          authentication: async () => {
+            return await authentication(req)
+          }
+        }
+      },
+      listen: {
+        port: process.env.PORT || 4000,
+      },
+    });
 
-  console.log(`🚀  Server ready at: ${url}`);
+    console.log(`🚀  Server ready at: ${url}`);
+  } catch (error) {
+    console.log(error);
+    
+  }
 })();
--- server/helpers/helpers.js
+++ server/helpers/helpers.js
@@ -1,5 +1,9 @@
 const { GraphQLError } = require("graphql");
+const jwt = require("jsonwebtoken")
+const jwtSecret = process.env.SECRET_KEY
+const bcrypt = require("bcryptjs")
 
+//Create Error
 function createError(message, code) {
   let httpMsg = "INTERNAL SERVER ERROR";
   if (code === 400) httpMsg = "BAD REQUEST";
@@ -11,6 +15,57 @@ function createError(message, code) {
   });
 }
 
+//Hash Password
+const hashPassword = (pass) => {
+  const salt = bcrypt.genSaltSync(10);
+  return bcrypt.hashSync(pass, salt)
+}
+
+const comparePassword = (pass, hashedPassword) => {
+  return bcrypt.compareSync(pass, hashedPassword)
+}
+
+//Set and Verify Token
+const setToken = (payload) => {
+  return jwt.sign(payload, jwtSecret)
+}
+
+const verifyToken = (token) => {
+  return jwt.verify(token, jwtSecret)
+}
+
+//Set Authentication
+const authentication = async (req) => {
+  const authorization = req.headers.authorization
+
+  if(!authorization) {
+    throw createError("Invalid Token", 401)
+  }
+
+  const token = authorization.split(" ")[1]
+
+  if(!token) {
+    throw new createError("Invalid Token", 401)
+  }
+
+  const decodeToken = verifyToken(token)
+  const user = await findUserById(decodeToken.id)
+  
+  if(!user) {
+    throw createError("Invalid User", 401)
+  }
+
+  return {
+    userId: user.id,
+    email: user.email
+  }
+}
+
 module.exports = {
   createError,
+  authentication,
+  setToken,
+  verifyToken,
+  hashPassword,
+  comparePassword
 };
