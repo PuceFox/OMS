4c49fb2c36db2761ae41a627a749eb5f788b8948
zakyabdurrahman <zakyabdurrahman10@gmail.com>
2024-08-06 15:06:42 +0700

    helpers and stuffs

--- server/helpers/helpers.js
+++ server/helpers/helpers.js
@@ -1,4 +1,6 @@
 const { GraphQLError } = require("graphql");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 function createError(message, code) {
   let httpMsg = "INTERNAL SERVER ERROR";
@@ -11,6 +13,22 @@ function createError(message, code) {
   });
 }
 
+function hashPassword(password) {
+  const salt = bcrypt.genSaltSync(10);
+  return bcrypt.hashSync(password, salt);
+}
+
+function comparePassword(password, hash) {
+  return bcrypt.compareSync(password, hash);
+}
+
+function createToken(payload) {
+  return jwt.sign(payload, process.env.JWT_SECRET);
+}
+
 module.exports = {
   createError,
+  comparePassword,
+  hashPassword,
+  createToken,
 };
