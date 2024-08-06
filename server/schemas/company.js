2148ee44b0129667b90a89a8b65dc02d36658f4c
zakyabdurrahman <zakyabdurrahman10@gmail.com>
2024-08-06 13:34:59 +0700

    setup sekeleton apollo

---
+++ server/schemas/company.js
@@ -0,0 +1,23 @@
+const typeDefs = `#graphql
+
+    type Company {
+        email: String,
+        password: String
+    }
+
+    type LoginResponse {
+        token: String,
+        oauthUrl: string
+    }
+
+
+    type Mutation {
+        login(email: String, password: String) : LoginResponse
+    
+    }
+
+`;
+
+module.exports = {
+  companyTypeDefs: typeDefs,
+};
