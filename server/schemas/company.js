aa23630023e28cbd20514de517a04ad7428829bd
zakyabdurrahman <zakyabdurrahman10@gmail.com>
2024-08-06 16:28:51 +0700

    login up

--- server/schemas/company.js
+++ server/schemas/company.js
@@ -1,3 +1,10 @@
+const {
+  createError,
+  setToken,
+  comparePassword,
+} = require("../helpers/helpers");
+const { findCompanyByEmail } = require("../models/company");
+
 const typeDefs = `#graphql
 
     type Company {
@@ -7,15 +14,55 @@ const typeDefs = `#graphql
 
     type LoginResponse {
         token: String,
-        oauthUrl: string
+        oauthUrl: String
     }
 
     type Mutation {
         login(email: String, password: String) : LoginResponse
     }
 
+    type Query {
+        getCompany(email: String) : Company
+    }
+
 `;
+
+const resolvers = {
+  Mutation: {
+    login: async function login(_parent, args) {
+      const { email, password } = args;
+
+      const company = await findCompanyByEmail(email);
+      if (!company) throw createError("Invalid Credential", 401);
+
+      //compare password
+      if (!comparePassword(password, company.password))
+        throw createError("Invalid Credential", 401);
+
+      const payload = {
+        companyId: company._id,
+        companyEmail: company.email,
+      };
+
+      const token = setToken(payload);
+
+      return {
+        token: token,
+        oauthUrl: "www",
+      };
+    },
+  },
+  Query: {
+    getCompany: async (_parent, args) => {
+      const { email } = args;
+      const company = await findCompanyByEmail(email);
+
+      return company;
+    },
+  },
+};
 
 module.exports = {
   companyTypeDefs: typeDefs,
+  companyResolvers: resolvers,
 };
