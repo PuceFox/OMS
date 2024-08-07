2de99d959dc86fb8924d8df47d37a8916a85467a
zakyabdurrahman <zakyabdurrahman10@gmail.com>
2024-08-07 15:28:45 +0700

    send email success

--- server/schemas/company.js
+++ server/schemas/company.js
@@ -7,6 +7,16 @@ const { findCompanyByEmail } = require("../models/company");
 const { authUrl } = require("../utils/oauthClient");
 const nodemailer = require("nodemailer");
 
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "79f807001@smtp-brevo.com",
+    pass: "PrdL4COMcgszbZRY",
+  },
+});
+
 const typeDefs = `#graphql
 
     type Company {
@@ -40,6 +50,17 @@ const resolvers = {
       //compare password
       if (!comparePassword(password, company.password))
         throw createError("Invalid Credential", 401);
+      try {
+        const info = await transporter.sendMail({
+          from: "<zakyabdurrahman10@gmail.com>",
+          to: "yoel.kristiadi.20@gmail.com",
+          subject: "Business Dealing",
+          text: "HAHA MAMPUS",
+          html: "<b>Hello world?</b>",
+        });
+      } catch (error) {
+        console.log(error);
+      }
 
       const payload = {
         companyId: company._id,
