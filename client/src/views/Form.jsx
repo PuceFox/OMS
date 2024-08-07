0b6fbf91346a582f679ce2a73ba27009c7a34cc9
Tiaraglo <siwutiara@gmail.com>
2024-08-07 14:42:13 +0700

    form and login

--- client/src/views/Form.jsx
+++ client/src/views/Form.jsx
@@ -1,7 +1,7 @@
 export default function Form() {
   return (
     <>
-      <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8 min-h-screen flex items-center justify-center">
         <div
           className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
           aria-hidden="true"
@@ -14,191 +14,146 @@ export default function Form() {
             }}
           />
         </div>
-        <div className="mx-auto max-w-2xl text-center">
+        <div className="relative z-10 mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Text{" "}
+            INPUT FORM
           </h2>
           <p className="mt-2 text-lg leading-8 text-gray-600">
-            Text who ever you want where ever you want any time you want enjoy
-            contacting freinds and families.
+            Please fill in your details.
           </p>
-        </div>
-        <form
-          action="#"
-          method="POST"
-          className="mx-auto mt-16 max-w-xl sm:mt-20"
-        >
-          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-            <div>
-              <label
-                htmlFor="first-name"
-                className="block text-sm font-semibold leading-6 text-black"
-              >
-                First name
-              </label>
-              <div className="mt-2.5">
-                <input
-                  type="text"
-                  name="first-name"
-                  id="first-name"
-                  autoComplete="given-name"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm shadow-blue-500 ring-1 ring-inset ring-blue-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-400 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div>
-              <label
-                htmlFor="last-name"
-                className="block text-sm font-semibold leading-6 text-black"
-              >
-                Last name
-              </label>
-              <div className="mt-2.5">
-                <input
-                  type="text"
-                  name="last-name"
-                  id="last-name"
-                  autoComplete="family-name"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset shadow-blue-500 ring-blue-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-400 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="company"
-                className="block text-sm font-semibold leading-6 text-black"
-              >
-                Company
-              </label>
-              <div className="mt-2.5">
-                <input
-                  type="text"
-                  name="company"
-                  id="company"
-                  autoComplete="organization"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset shadow-blue-500 ring-blue-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-400 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="email"
-                className="block text-sm font-semibold leading-6 text-red-600"
-              >
-                Email
-              </label>
-              <div className="mt-2.5">
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  autoComplete="email"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset shadow-blue-500 ring-blue-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-400 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="phone-number"
-                className="block text-sm font-semibold leading-6 text-green-600"
-              >
-                Phone number
-              </label>
-              <div className="relative mt-2.5">
-                <div className="absolute inset-y-0 left-0 flex items-center">
-                  <label htmlFor="country" className="sr-only">
-                    Country
-                  </label>
-                  <select
-                    id="country"
-                    name="country"
-                    className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
-                  >
-                    <option>US</option>
-                    <option>CA</option>
-                    <option>EU</option>
-                    <option>UK</option>
-                    <option>YE</option>
-                    <option>MA</option>
-                  </select>
-                  <svg
-                    className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </div>
-                <input
-                  type="tel"
-                  name="phone-number"
-                  id="phone-number"
-                  autoComplete="tel"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="message"
-                className="block text-sm font-semibold leading-6 text-green-600"
-              >
-                Message
-              </label>
-              <div className="mt-2.5">
-                <textarea
-                  name="message"
-                  id="message"
-                  rows={4}
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  defaultValue={""}
-                />
-              </div>
-            </div>
-            <div className="flex gap-x-4 sm:col-span-2">
-              <div className="flex h-6 items-center">
-                {/* Enabled: "bg-indigo-600", Not Enabled: "bg-gray-200" */}
-                <button
-                  type="button"
-                  className="bg-gray-200 flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  role="switch"
-                  aria-checked="false"
-                  aria-labelledby="switch-1-label"
+          <form
+            action="#"
+            method="POST"
+            className="mx-auto mt-8 max-w-xl space-y-6"
+          >
+            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="full-name"
+                  className="block text-lg font-semibold leading-6 text-black"
                 >
-                  <span className="sr-only">Agree to policies</span>
-                  {/* Enabled: "translate-x-3.5", Not Enabled: "translate-x-0" */}
-                  <span
-                    aria-hidden="true"
-                    className="translate-x-0 h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"
+                  Full Name
+                </label>
+                <div className="mt-3">
+                  <input
+                    type="text"
+                    name="full-name"
+                    id="full-name"
+                    autoComplete="name"
+                    className="block w-full rounded-md border-0 px-4 py-3 text-gray-900 shadow-sm ring-1 ring-inset shadow-blue-500 ring-blue-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-400 text-lg"
                   />
-                </button>
+                </div>
               </div>
-              <label
-                className="text-sm leading-6 text-gray-600"
-                id="switch-1-label"
-              >
-                By selecting this free palestine
-                <a href="#" className="font-semibold text-red-600">
-                  privacy&nbsp;policy
-                </a>
-                .
-              </label>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="email"
+                  className="block text-lg font-semibold leading-6 text-black"
+                >
+                  Email
+                </label>
+                <div className="mt-3">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    autoComplete="email"
+                    className="block w-full rounded-md border-0 px-4 py-3 text-gray-900 shadow-sm ring-1 ring-inset shadow-blue-500 ring-blue-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-400 text-lg"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="phone-number"
+                  className="block text-lg font-semibold leading-6 text-black"
+                >
+                  Phone Number
+                </label>
+                <div className="mt-3">
+                  <input
+                    type="tel"
+                    name="phone-number"
+                    id="phone-number"
+                    autoComplete="tel"
+                    className="block w-full rounded-md border-0 px-4 py-3 text-gray-900 shadow-sm ring-1 ring-inset shadow-blue-500 ring-blue-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-400 text-lg"
+                  />
+                </div>
+              </div>
+              <div className="flex gap-x-8 sm:col-span-2">
+                <div className="flex-1">
+                  <label
+                    htmlFor="origin"
+                    className="block text-lg font-semibold leading-6 text-black"
+                  >
+                    Origin
+                  </label>
+                  <div className="relative mt-3">
+                    <select
+                      id="origin"
+                      name="origin"
+                      className="block w-full rounded-md border-0 py-3 pl-4 pr-10 text-gray-900 shadow-sm ring-1 ring-inset shadow-blue-500 ring-blue-300 focus:ring-2 focus:ring-inset focus:ring-blue-400 text-lg"
+                    >
+                      <option value="">Select Origin</option>
+                      <option value="US">US</option>
+                      <option value="CA">CA</option>
+                      <option value="EU">EU</option>
+                      <option value="UK">UK</option>
+                      <option value="YE">YE</option>
+                      <option value="MA">MA</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="flex-1">
+                  <label
+                    htmlFor="destination"
+                    className="block text-lg font-semibold leading-6 text-black"
+                  >
+                    Destination
+                  </label>
+                  <div className="relative mt-3">
+                    <select
+                      id="destination"
+                      name="destination"
+                      className="block w-full rounded-md border-0 py-3 pl-4 pr-10 text-gray-900 shadow-sm ring-1 ring-inset shadow-blue-500 ring-blue-300 focus:ring-2 focus:ring-inset focus:ring-blue-400 text-lg"
+                    >
+                      <option value="">Select Destination</option>
+                      <option value="US">US</option>
+                      <option value="CA">CA</option>
+                      <option value="EU">EU</option>
+                      <option value="UK">UK</option>
+                      <option value="YE">YE</option>
+                      <option value="MA">MA</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="company"
+                  className="block text-lg font-semibold leading-6 text-black"
+                >
+                  Total Passangers
+                </label>
+                <div className="mt-3">
+                  <input
+                    type="text"
+                    name="company"
+                    id="company"
+                    autoComplete="organization"
+                    className="block w-full rounded-md border-0 px-4 py-3 text-gray-900 shadow-sm ring-1 ring-inset shadow-blue-500 ring-blue-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-400 text-lg"
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="mt-10">
-            <button
-              type="submit"
-              className="block w-full rounded-md bg-cyan-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Let's talk
-            </button>
-          </div>
-        </form>
+            <div className="mt-8">
+              <button
+                type="submit"
+                className="block w-full rounded-md bg-cyan-600 px-4 py-3 text-center text-lg font-semibold text-white shadow-sm hover:bg-cyan-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
