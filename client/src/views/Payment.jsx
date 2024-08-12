3a6457c0d88ad3a42aa0cb0fc62cd45d3c3b263c
Tiaraglo <siwutiara@gmail.com>
2024-08-12 15:45:29 +0700

    page for payment succes and payment status

--- client/src/views/Payment.jsx
+++ client/src/views/Payment.jsx
@@ -9,6 +9,10 @@ import {
 } from "@stripe/react-stripe-js";
 import { REST_API_URL } from "../constant/constant";
 
+import { motion } from "framer-motion";
+
+import logo from "../assets/name.png";
+
 export default function Payment() {
   const params = useParams();
   const stripePromise = loadStripe(
@@ -24,23 +28,42 @@ export default function Payment() {
   });
 
   const fetchClientSecret = useCallback(() => {
-    // Create a Checkout Session
     return fetch(`${REST_API_URL}/create-checkout-session/${orderId}`, {
       method: "POST",
     })
       .then((res) => res.json())
       .then((data) => data.clientSecret);
-  }, []);
+  }, [orderId]);
 
   const options = { fetchClientSecret };
 
   return (
     <>
-      <div>
-        <h1>PAYMENT PAGE</h1>
+      <nav className="fixed top-0 left-0 right-0 bg-gradient-to-r from-purple-700 via-purple-800 to-purple-900 text-white shadow-lg z-20">
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          className="container mx-auto px-4 py-4 flex justify-between items-center max-w-7xl"
+        >
+          <div className="flex items-center gap-2">
+            <div className="w-8 h-8 bg-white rounded-full flex items-center justify-center">
+              <span className="text-purple-800 font-bold text-xl">O</span>
+            </div>
+            <h1 className="font-bold text-2xl text-white">Orderly</h1>
+          </div>
+        </motion.div>
+      </nav>
+      <div className="relative w-full h-screen bg-gradient-to-r from-blue-300 via-indigo-600 to-purple-800 opacity-80">
+        <br />
+        <h1 className="text-center mt-16 text-6xl font-serif mb-8 bg-gradient-to-r from-purple-500 via-blue-200 to-blue-500 bg-clip-text text-transparent shadow-lg stroke-effect">
+          PAYMENT PAGE
+        </h1>
         <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
           <EmbeddedCheckout />
         </EmbeddedCheckoutProvider>
+        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-full flex justify-center z-30">
+          <img src={logo} alt="Logo" className="w-4/12 t" />
+        </div>
       </div>
     </>
   );
