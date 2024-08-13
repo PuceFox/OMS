d719223ae82850af52da2c8637b5f5d8120765f9
arifghiffari <MAGhiffari20@gmail.com>
2024-08-13 10:58:02 +0700

    handle base layout

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -7,25 +7,10 @@ import logo from "../assets/name.png";
 import Toastify from "toastify-js";
 import { useEffect, useState } from "react";
 
-const TABLE_HEAD = [
-  "Fullname",
-  "Email",
-  "Phone Number",
-  "Origin",
-  "Destination",
-  "Services",
-  "Total Pax",
-  "Total Price",
-  "Aircraft",
-  "Status",
-  "Action",
-];
+const TABLE_HEAD = ["Name", "Email", "Phone Number", "Origin", "Destination", "Services", "Total Pax", "Total Price", "Aircraft", "Status", "Action"];
 
 export default function Dashboard() {
-  const [
-    fetchOrders,
-    { loading: tableLoading, error: tableError, data: tableData },
-  ] = useLazyQuery(QUERY_GET_ORDERS);
+  const [fetchOrders, { loading: tableLoading, error: tableError, data: tableData }] = useLazyQuery(QUERY_GET_ORDERS);
   const [followUp] = useMutation(MUTATION_FOLLOW_UP, {
     onError: (error) => {
       console.error("Mutation Error:", error);
@@ -83,15 +68,8 @@ export default function Dashboard() {
             <thead>
               <tr>
                 {TABLE_HEAD.map((head) => (
-                  <th
-                    key={head}
-                    className="border-b border-blue-gray-100 bg-purple-300 p-4"
-                  >
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-bold leading-none text-white"
-                    >
+                  <th key={head} className="border-b border-blue-gray-100 bg-purple-300 p-4">
+                    <Typography variant="small" color="blue-gray" className="font-bold leading-none text-white">
                       {head}
                     </Typography>
                   </th>
@@ -99,177 +77,85 @@ export default function Dashboard() {
               </tr>
             </thead>
             <tbody>
-              {tableRows?.map(
-                (
-                  {
-                    _id,
-                    fullname,
-                    email,
-                    phoneNumber,
-                    origin,
-                    destination,
-                    service,
-                    pax,
-                    price,
-                    aircraft,
-                    status,
-                  },
-                  index
-                ) => {
-                  const isLast = index === tableRows.length - 1;
-                  const classes = isLast
-                    ? "p-4"
-                    : "p-4 border-b border-blue-gray-50";
+              {tableRows?.map(({ _id, fullname, email, phoneNumber, origin, destination, service, pax, price, aircraft, status }, index) => {
+                const isLast = index === tableRows.length - 1;
+                const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
 
-                  return (
-                    <tr key={fullname}>
-                      <td className={classes}>
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal"
-                        >
-                          {fullname}
+                return (
+                  <tr key={fullname}>
+                    <td className={classes}>
+                      <Typography variant="small" color="blue-gray" className="font-normal">
+                        {fullname}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography variant="small" color="blue-gray" className="font-normal">
+                        {email}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography variant="small" color="blue-gray" className="font-normal">
+                        {phoneNumber}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography as="a" href="#" variant="small" color="blue-gray" className="font-medium">
+                        {origin}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography as="a" href="#" variant="small" color="blue-gray" className="font-medium">
+                        {destination}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography as="a" href="#" variant="small" color="blue-gray" className="font-medium">
+                        {service}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      <Typography as="a" href="#" variant="small" color="blue-gray" className="font-medium">
+                        {pax}
+                      </Typography>
+                    </td>
+                    <td className={classes}>
+                      {price ? (
+                        <Typography as="a" href="#" variant="small" color="blue-gray" className="font-medium">
+                          {formatPrice(price)}
                         </Typography>
-                      </td>
-                      <td className={classes}>
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal"
-                        >
-                          {email}
+                      ) : (
+                        <Typography as="a" href="#" variant="small" color="blue-gray" className="font-medium">
+                          -
                         </Typography>
-                      </td>
-                      <td className={classes}>
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal"
-                        >
-                          {phoneNumber}
-                        </Typography>
-                      </td>
-                      <td className={classes}>
-                        <Typography
-                          as="a"
-                          href="#"
-                          variant="small"
-                          color="blue-gray"
-                          className="font-medium"
-                        >
-                          {origin}
-                        </Typography>
-                      </td>
-                      <td className={classes}>
-                        <Typography
-                          as="a"
-                          href="#"
-                          variant="small"
-                          color="blue-gray"
-                          className="font-medium"
-                        >
-                          {destination}
-                        </Typography>
-                      </td>
-                      <td className={classes}>
-                        <Typography
-                          as="a"
-                          href="#"
-                          variant="small"
-                          color="blue-gray"
-                          className="font-medium"
-                        >
-                          {service}
-                        </Typography>
-                      </td>
-                      <td className={classes}>
-                        <Typography
-                          as="a"
-                          href="#"
-                          variant="small"
-                          color="blue-gray"
-                          className="font-medium"
-                        >
-                          {pax}
-                        </Typography>
-                      </td>
-                      <td className={classes}>
-                        {price ? (
-                          <Typography
-                            as="a"
-                            href="#"
-                            variant="small"
-                            color="blue-gray"
-                            className="font-medium"
-                          >
-                            {formatPrice(price)}
-                          </Typography>
-                        ) : (
-                          <Typography
-                            as="a"
-                            href="#"
-                            variant="small"
-                            color="blue-gray"
-                            className="font-medium"
-                          >
-                            -
-                          </Typography>
-                        )}
-                      </td>
-                      <td className={classes}>
-                        {aircraft ? (
-                          <Typography
-                            as="a"
-                            href="#"
-                            variant="small"
-                            color="blue-gray"
-                            className="font-medium"
-                          >
-                            {aircraft}
-                          </Typography>
-                        ) : (
-                          <Typography
-                            as="a"
-                            href="#"
-                            variant="small"
-                            color="blue-gray"
-                            className="font-medium"
-                          >
-                            Waiting
-                          </Typography>
-                        )}
-                      </td>
-                      <td className={classes}>
-                        <Typography
-                          as="a"
-                          href="#"
-                          variant="small"
-                          color="blue-gray"
-                          className="font-medium"
-                        >
-                          {status}
-                        </Typography>
-                      </td>
-
-                      {status !== "Accepted" && status !== "Rejected" && (
-                        <td className={classes}>
-                          <Button
-                            as="a"
-                            href="#"
-                            variant="small"
-                            color="amber"
-                            className="font-bold"
-                            onClick={() => handleFollowUp(_id)}
-                          >
-                            Follow Up
-                          </Button>
-                        </td>
                       )}
-                    </tr>
-                  );
-                }
-              )}
+                    </td>
+                    <td className={classes}>
+                      {aircraft ? (
+                        <Typography as="a" href="#" variant="small" color="blue-gray" className="font-medium">
+                          {aircraft}
+                        </Typography>
+                      ) : (
+                        <Typography as="a" href="#" variant="small" color="blue-gray" className="font-medium">
+                          Waiting
+                        </Typography>
+                      )}
+                    </td>
+                    <td className={classes}>
+                      <Typography as="a" href="#" variant="small" color="blue-gray" className="font-medium">
+                        {status}
+                      </Typography>
+                    </td>
+
+                    {status !== "Accepted" && status !== "Rejected" && (
+                      <td className={classes}>
+                        <Button as="a" href="#" variant="small" color="amber" className="font-bold" onClick={() => handleFollowUp(_id)}>
+                          Follow Up
+                        </Button>
+                      </td>
+                    )}
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </Card>
