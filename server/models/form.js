c5725aeb928a460a42a65d047c79061682624323
Naufal Jundi <167834441+jundinaufal@users.noreply.github.com>
2024-08-14 18:04:18 +0700

    add paid in chart

--- server/models/form.js
+++ server/models/form.js
@@ -83,7 +83,8 @@ async function findDataAI() {
 // Function untuk Get Data Percentage dari Status
 async function findPecentage() {
   const orderReject = await findOrderByStatus("Rejected");
-  const orderAccept = await findOrderByStatus("Paid");
+  const orderAccept = await findOrderByStatus("Accepted");
+  const orderPaid = await findOrderByStatus("Paid");
   const orderPending = await findOrderByStatus("Pending");
   const orderNegotiate = await findOrderByStatus("Negotiation");
   const totalOrder =
@@ -95,6 +96,7 @@ async function findPecentage() {
   return {
     totalReject: orderReject.length,
     totalAccept: orderAccept.length,
+    totalPaid: orderPaid.length,
     totalPending: orderPending.length,
     totalNego: orderNegotiate.length,
     totalRequest: totalOrder,
